3c6fe0aef5c6780802db5575ec12ac48506907eb
rohitkandhal <2447448+rohitkandhal@users.noreply.github.com>
2022-09-02 12:24:05 -0700

    accumulator

---
+++ src/js-concepts/9-pipe.js
@@ -0,0 +1,32 @@
+// create a pipe() function, which chains multiple functions together to create a new function.
+
+const times = (y) => (x) => x * y
+const plus = (y) => (x) => x + y
+const subtract = (y) => (x) => x - y
+const divide = (y) => (x) => x / y
+
+function pipeForOneFunction(funcs) {
+  return function (arg) {
+    return funcs(arg)
+  }
+}
+const multiple = pipeForOneFunction(
+  times(2)
+)
+
+function pipe(funcs) {
+  return function (arg) {
+    return funcs.reduce((accumulator, currFun) => {
+      return currFun(accumulator)
+    }, arg)
+  }
+}
+
+const multiples = pipe([
+  times(2),
+  times(3),
+  times(2)
+])
+// x * 2 * 3
+
+console.log(`5*2*3 = ${multiples(5)}`);

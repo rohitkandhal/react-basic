e1ee02c9e5ea452d8b8fa7280a7f544bcbd1b8c3
rohitkandhal <2447448+rohitkandhal@users.noreply.github.com>
2022-09-03 09:32:27 -0700

    Event emitter

--- src/js-concepts/23-eventSubscriberAndEmitter.js
+++ src/js-concepts/23-eventSubscriberAndEmitter.js
@@ -33,18 +33,18 @@ class EventEmitter {
   }
 }
 
-const emitter = new Emitter()
-const sub1 = emitter.subscribe('event1', callback1)
-const sub2 = emitter.subscribe('event2', callback2)
+// const emitter = new Emitter()
+// const sub1 = emitter.subscribe('event1', callback1)
+// const sub2 = emitter.subscribe('event2', callback2)
 
-// same callback could subscribe 
-// on same event multiple times
-const sub3 = emitter.subscribe('event1', callback1)
+// // same callback could subscribe 
+// // on same event multiple times
+// const sub3 = emitter.subscribe('event1', callback1)
 
-emitter.emit('event1', 1, 2);
-// callback1 will be called twice
+// emitter.emit('event1', 1, 2);
+// // callback1 will be called twice
 
-sub1.release()
-sub3.release()
-// now even if we emit 'event1' again,
-// callback1 is not called anymore
+// sub1.release()
+// sub3.release()
+// // now even if we emit 'event1' again,
+// // callback1 is not called anymore
